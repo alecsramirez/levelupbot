e83e6de336d48d2ac7b39b6b423916ea95512b9b
Alecs Ramirez <alecsbramirez@gmail.com>
2020-05-10 21:05:58 +0100

    Added bot.py and the command handler

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+import components.handler
+from importlib import reload
+
+admins = []
+with open("secrets/admins.txt") as adminFile:
+    admins = [int(line.strip()) for line in adminFile.readlines()]
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "!reload" and message.author.id in admins:
+        reload(components.handler)
+        
+    pass
+
+if __name__ == "__name__":
+    with open("secrets/token.txt") as token:
+        client.run(token.read().strip())
