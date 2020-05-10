8fbb8a2e704c6e73b0c24e500dd5c32cec70c12b
Alecs Ramirez <alecsbramirez@gmail.com>
2020-05-10 21:11:53 +0100

    Updated bot.py to include reloading the handler to add features without a bot restart

--- bot.py
+++ bot.py
@@ -14,8 +14,9 @@ async def on_message(message):
         return
     if message.content == "!reload" and message.author.id in admins:
         reload(components.handler)
-        
-    pass
+        return
+
+    components.handler.Handler().handle(message, client)
 
 if __name__ == "__name__":
     with open("secrets/token.txt") as token:
--- components/handler.py
+++ components/handler.py
@@ -1,3 +1,8 @@
+import discord
+
 class Handler:
     def __init__(self):
+        pass
+
+    def handle(self, message: discord.Message, bot: discord.Client):
         pass
